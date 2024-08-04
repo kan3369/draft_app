5d64cae7d272f8e84bbaa12053c5b8f8734d2ee8
RyoheiUtsumi <utsumi.job@gmail.com>
2024-08-04 09:49:35 +0900

    delete.php

--- server/document/delete.php
+++ server/document/delete.php
@@ -1,49 +1,38 @@
 <?php
+// 必要なファイルを読み込む
 require_once __DIR__ . '/../common/functions.php';
 
-$message = ''; // $message 変数を初期化
+// 出力バッファリングを開始する
+ob_start();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // POSTリクエストからIDを取得
-    $doc_id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+// セッション開始
+session_start();
 
-    if (!$doc_id) {
-        $message = 'IDが不正です。';
-        exit;
-    }
-    try {
+// POSTリクエストからIDを取得
+$doc_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-    $dbh = connect_db();
-        // docテーブルからデータを削除するSQL
-        $sql = 'DELETE FROM doc WHERE id = :id';
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindParam(':id', $doc_id, PDO::PARAM_INT);
-        $stmt->execute();
-
-        $message = "データが正常に削除されました。";
-    } catch (PDOException $e) {
-        $message = '接続失敗: ' . $e->getMessage();
-    }
-} else {
-    $message = '不正なアクセスです。';
+if (!$doc_id) {
+    $_SESSION['message'] = 'IDが不正です。';
+    header('Location: /document/index.php');
+    exit;
 }
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Delete Page</title>
-</head>
-<body>
+try {
+    $dbh = connect_db();
+    // docテーブルからデータを削除するSQL
+    $sql = 'DELETE FROM doc WHERE id = :id';
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':id', $doc_id, PDO::PARAM_INT);
+    $stmt->execute();
 
-<h1>削除結果</h1>
+    $_SESSION['message'] = "データが正常に削除されました。";
+} catch (PDOException $e) {
+    $_SESSION['message'] = '接続失敗: ' . $e->getMessage();
+}
 
-<p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8') ?></p>
-<?php if (isset($doc_id) && $message === "データが正常に削除されました。") : ?>
-    <p>ID: <?= htmlspecialchars($doc_id, ENT_QUOTES, 'UTF-8') ?> のデータが削除されました。</p>
-<?php endif; ?>
+// 一覧画面にリダイレクト
+header('Location: /document/index.php');
+exit;
 
-<a href="index.php">戻る</a>
-
-</body>
-</html>
+// 出力バッファリングを終了する
+ob_end_flush();
