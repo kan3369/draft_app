8743296601174389f98e44c9e8f0a842015edb89
Wald-japan <shimoigusa.kobetsu@gmail.com>
2024-08-04 09:04:06 +0900

    create_css

--- server/document/create.php
+++ server/document/create.php
@@ -51,6 +51,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>作成</title>
+    <link rel="stylesheet" href="/../css/sttly.css">
 </head>
 
 <body>
